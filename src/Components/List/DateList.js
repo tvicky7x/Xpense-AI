ec8bd2a4bc0c0eba3c1b79d4bea3caf1ab25ba7d
tvicky7x <tvicky7x@gmail.com>
2023-09-11 20:47:32 +0530

    correction.

--- src/Components/List/DateList.js
+++ src/Components/List/DateList.js
@@ -4,14 +4,14 @@ import ListItem from "./ListItem";
 function DateList({ date, list }) {
   return (
     <>
-      <div className=" border-b-2 px-1 sm:px-2 pb-1 text-slate-500 mt-1.5 border-slate-300 newFont sm:text-lg">
+      <div className=" border-b-2 mx-1 sm:mx-2 pb-1 text-slate-500 mt-1.5 border-slate-300 newFont sm:text-lg">
         <p>{`${new Date(date).toLocaleDateString(undefined, {
           weekday: "long",
           month: "short",
           day: "numeric",
         })}`}</p>
       </div>
-      <ul className=" divide-y divide-slate-200">
+      <ul className=" divide-y divide-slate-200 px-3 sm:px-4">
         {list.map((expense) => {
           return <ListItem expense={expense} key={expense.id} />;
         })}
